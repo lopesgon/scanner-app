4099273226ad60b8705654bb7b45d9ba0fbadcc4
Frederic Lopes <mag.frederic@gmail.com>
2020-10-11 11:10:23 +0200

    Fixed camera and added camera trigger button

--- App.tsx
+++ App.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { ShowNames } from './src/components/show-names.component';
+import { styles } from './styles/styles';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { HomeScreen } from './src/screens/home.screen';
+import { ScannerCamera } from './src/components/scanner-camera.component';
+import { SettingsScreen } from './src/screens/settings.screen';
+
+const BottomTabs = createBottomTabNavigator();
+
+export class Default extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScannerCamera></ScannerCamera>
+      </View>
+    )
+  }
+}
+
+export interface IDefaultProps {
+  value: any
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <BottomTabs.Navigator>
+        <BottomTabs.Screen name="Home" component={HomeScreen} />
+        <BottomTabs.Screen name="Settings" component={SettingsScreen} />
+      </BottomTabs.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
